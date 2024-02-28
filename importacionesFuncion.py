ccd9abd64f26833b23510d2951033ed891f77717
gabriel270613 <gmora9079@gmail.com>
2024-02-27 23:13:09 -0600

    aver que pasa

---
+++ importacionesFuncion.py
@@ -0,0 +1,10 @@
+import FuncionSuma
+
+numero1 = 2
+numero2 = 2
+numero3 = 2
+
+total = FuncionSuma.suma(numero1,numero2)
+print(total)
+total = FuncionSuma.multiplicacion(numero1,numero2,numero3)
+print(total)
